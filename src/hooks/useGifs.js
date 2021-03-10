7f36103d1f73fcd43a04b84f9d7fa5d5e76b1f1f
nicohnavarro <nicohnavarro@gmail.com>
2021-03-10 13:08:58 -0300

    :art: hook

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../services/getGifs";
 
-
 export function useGifs({ keyword }) {
   const [loading, setLoading] = useState(false);
   const [gifs, setGifs] = useState([]);
